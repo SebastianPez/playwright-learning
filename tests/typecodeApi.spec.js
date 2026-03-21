5e95c833c161d378381c970ca630431dd19718af
Sebastian <sebastian.pezarro@gmail.com>
2026-03-20 19:47:37 -0700

    Updating API test for new endponts

---
+++ tests/typecodeApi.spec.js
@@ -0,0 +1,27 @@
+const { test, expect } = require('@playwright/test');
+
+test('GET /posts should return 200', async ({ request }) => {
+    const response = await request.get('https://jsonplaceholder.typicode.com/posts/1');
+
+    expect(response.status()).toBe(200);
+
+    const body = await response.json();
+    expect(body.id).toBe(1);
+});
+
+test('POST /posts should create a post', async ({ request }) => {
+    const response = await response.post('https://jsonplaceholder.typicode.com/posts', {
+        data: {
+            title: 'foo',
+            body: 'bar',
+            userId: 1
+        },
+    })
+
+
+expect(response.status()).toBe(201);
+
+const body = await response.json();
+expect(body.title).toBe('foo');
+
+});
