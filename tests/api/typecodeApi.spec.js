977df778fb59607e3ea3f131ba7c90811c41832c
Sebastian <sebastian.pezarro@gmail.com>
2026-03-21 12:10:27 -0700

    Wrapping positive tests

--- tests/api/typecodeApi.spec.js
+++ tests/api/typecodeApi.spec.js
@@ -1,29 +1,32 @@
 const { test, expect } = require('@playwright/test');
 
-test('GET /posts should return 200', async ({ request }) => {
-    const response = await request.get('/posts/1');
+test.describe('POSITIVE tests', () => {
 
-    expect(response.status()).toBe(200);
+    test('GET /posts should return 200', async ({ request }) => {
+        const response = await request.get('/posts/1');
+
+        expect(response.status()).toBe(200);
+
+        const body = await response.json();
+        expect(body.id).toBe(1);
+    });
+
+    test('POST /posts should create a post', async ({ request }) => {
+        const response = await request.post('/posts', {
+
+            data: {
+                title: 'foo',
+                body: 'bar',
+                userId: 1
+            },
+        })
+
+    expect(response.status()).toBe(201);
 
     const body = await response.json();
-    expect(body.id).toBe(1);
-});
+    expect(body.title).toBe('foo');
 
-test('POST /posts should create a post', async ({ request }) => {
-    const response = await request.post('/posts', {
-
-        data: {
-            title: 'foo',
-            body: 'bar',
-            userId: 1
-        },
-    })
-
-expect(response.status()).toBe(201);
-
-const body = await response.json();
-expect(body.title).toBe('foo');
-
+    });
 });
 
 test.describe('NEGATIVE tests', () => {
